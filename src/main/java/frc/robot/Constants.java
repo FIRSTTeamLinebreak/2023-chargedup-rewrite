6dd6e492cd0190b6c77e62a4fd87d272edbf26cc
ItsNoellaHere <49508517+ItsNoellaHere@users.noreply.github.com>
2023-11-10 19:51:01 -0700

    added libs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,6 +17,30 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  /** Assorted other constants for the swerve subsystem. */
+  public static final class SwerveConstants {
+    public static final double wheelDiameter = Units.inchesToMeters(2); // Meters
+    public static final double driveGearRatio = 6.12;
+
+    public static final double driveRotToMeters = ((2 * Math.PI * wheelDiameter) / driveGearRatio) / 2048; // Drive motor rotations to meters
+    public static final double driveRpsToMps = driveRotToMeters / 10 * 60 * 2; // Drive motor rotations per second to meters per second
+
+    public static final double drivePhysicalMaxSpeed = 5.486; // Physical max speed of the motor in m/s
+
+    public static final double trackWidth = Units.inchesToMeters(23.5); // Distance between the center of the left and right wheels in meters
+    public static final double wheelBase = Units.inchesToMeters(21.5); // Distance between the center of the front and back wheels in meters
+    public static final Translation2d locationFL = new Translation2d(wheelBase / 2,  trackWidth / 2);
+    public static final Translation2d locationFR = new Translation2d(wheelBase / 2, -trackWidth / 2);
+    public static final Translation2d locationBL = new Translation2d(-wheelBase / 2,  trackWidth / 2);
+    public static final Translation2d locationBR = new Translation2d(-wheelBase / 2, -trackWidth / 2);
+    public static final SwerveDriveKinematics driveKinematics = new SwerveDriveKinematics(
+        locationFL,
+        locationFR,
+        locationBL,
+        locationBR
+    );
+  }
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
